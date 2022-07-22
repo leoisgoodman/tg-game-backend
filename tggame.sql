e404eabcb70fb628bf14337a2ccc2e1e4931e011
watson <watson@cg.support>
2022-07-22 15:50:03 +0800

    init

--- tggame.sql
+++ tggame.sql
@@ -231,9 +231,7 @@ CREATE TABLE `worker_node`
     `PORT`        varchar(64) NOT NULL COMMENT 'port',
     `TYPE`        int(11)     NOT NULL COMMENT 'node type: ACTUAL or CONTAINER',
     `LAUNCH_DATE` date        NOT NULL COMMENT 'launch date',
-    `MODIFIED`    timestamp   NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'modified time',
-    `CREATED`     timestamp   NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'created time',
+    `MODIFIED`    timestamp   NOT NULL COMMENT 'modified time',
+    `CREATED`     timestamp   NOT NULL COMMENT 'created time',
     PRIMARY KEY (`ID`)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 1263
-  DEFAULT CHARSET = utf8 COMMENT ='DB WorkerID Assigner for UID Generator';
+) DEFAULT CHARSET = utf8 COMMENT ='DB WorkerID Assigner for UID Generator';
