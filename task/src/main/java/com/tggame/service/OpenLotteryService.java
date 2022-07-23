1ae035bb9e2ef74705fd84fe1b879baa5c215307
watson <watson@cg.support>
2022-07-23 20:17:42 +0800

    打开走势图

--- task/src/main/java/com/tggame/service/OpenLotteryService.java
+++ task/src/main/java/com/tggame/service/OpenLotteryService.java
@@ -24,72 +24,78 @@ import java.util.Date;
 @Service
 public class OpenLotteryService {
 
-  @Autowired private OpenRecordService openRecordService;
+    @Autowired
+    private OpenRecordService openRecordService;
 
-  @Autowired private ApplicationEventPublisher applicationEventPublisher;
+    @Autowired
+    private ApplicationEventPublisher applicationEventPublisher;
 
-  @Autowired private UidGenerator uidGenerator;
+    @Autowired
+    private UidGenerator uidGenerator;
 
-  @Autowired private RedisServiceSVImpl redisServiceSV;
+    @Autowired
+    private RedisServiceSVImpl redisServiceSV;
 
-  /** 实现开奖业务 1.ws拉取奖源结果 2.保存开奖记录 3.开启下一期并生成新的一起进行ready状态 4.统计走势 */
-  @Transactional(rollbackFor = Exception.class)
-  public void open() throws Exception {
-    try {
-      Thread.sleep(5000);
-    } catch (InterruptedException e) {
-      throw new Exception(e);
+    /**
+     * 实现开奖业务 1.ws拉取奖源结果 2.保存开奖记录 3.开启下一期并生成新的一起进行ready状态 4.统计走势
+     */
+    @Transactional(rollbackFor = Exception.class)
+    public void open() throws Exception {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new Exception(e);
+        }
+        // 1.ws拉取奖源结果
+        Long time =
+                new DateTime(DateUtil.truncate(DateUtil.calendar(new Date()), DateField.MINUTE)).getTime();
+
+        String btcKey = RedisKey.genBTCKey(time);
+        String num = (String) redisServiceSV.get(btcKey);
+        log.info("获取到的BTC的价格为:{}", num);
+
+        // 2.更新开奖记录  状态是为封盘的
+        OpenRecord openRecord =
+                openRecordService.getOne(
+                        new LambdaQueryWrapper<OpenRecord>()
+                                .eq(OpenRecord::getStatus, OpenRecordStatus.Lock)
+                                .orderByDesc(OpenRecord::getIssue)
+                                .last("limit 1"));
+
+        openRecord.setNum(num);
+        openRecord.setStatus(OpenRecordStatus.Drawn.name());
+        openRecord.setUpdateTime(new Date());
+        openRecordService.updateById(openRecord);
+
+        // 3.开启下一期并生成新的一起进行Enable状态
+        openRecordService.save(
+                OpenRecord.builder()
+                        .id(String.valueOf(uidGenerator.getUID()))
+                        .issue(
+                                Long.parseLong(
+                                        DateUtil.format(new Date(), DatePattern.PURE_DATETIME_PATTERN)
+                                                .substring(0, 12)))
+                        .status(OpenRecordStatus.Ready.name())
+                        .createTime(new Date())
+                        .updateTime(new Date())
+                        .build());
+
+        // 4.统计走势
+        applicationEventPublisher.publishEvent(new TrendBuildEvent(this, openRecord));
     }
-    // 1.ws拉取奖源结果
-    Long time =
-        new DateTime(DateUtil.truncate(DateUtil.calendar(new Date()), DateField.MINUTE)).getTime();
 
-    String btcKey = RedisKey.genBTCKey(time);
-    String num = (String) redisServiceSV.get(btcKey);
-    log.info("获取到的BTC的价格为:{}", num);
+    @Transactional(rollbackFor = Exception.class)
+    public void lock() throws Exception {
+        // 查询最新的可投注数据
+        OpenRecord openRecord =
+                openRecordService.getOne(
+                        new LambdaQueryWrapper<OpenRecord>()
+                                .eq(OpenRecord::getStatus, OpenRecordStatus.Ready)
+                                .orderByDesc(OpenRecord::getIssue)
+                                .last("limit 1"));
 
-    // 2.更新开奖记录  状态是为封盘的
-    OpenRecord openRecord =
-        openRecordService.getOne(
-            new LambdaQueryWrapper<OpenRecord>()
-                .eq(OpenRecord::getStatus, OpenRecordStatus.Lock)
-                .orderByDesc(OpenRecord::getIssue)
-                .last("limit 1"));
-
-    openRecord.setNum(num);
-    openRecord.setStatus(OpenRecordStatus.Drawn.name());
-    openRecord.setUpdateTime(new Date());
-    openRecordService.updateById(openRecord);
-
-    // 3.开启下一期并生成新的一起进行Enable状态
-    openRecordService.save(
-        OpenRecord.builder()
-            .id(String.valueOf(uidGenerator.getUID()))
-            .issue(
-                Long.parseLong(
-                    DateUtil.format(new Date(), DatePattern.PURE_DATETIME_PATTERN)
-                        .substring(0, 12)))
-            .status(OpenRecordStatus.Ready.name())
-            .createTime(new Date())
-            .updateTime(new Date())
-            .build());
-
-    // 4.统计走势
-    //        applicationEventPublisher.publishEvent(new TrendBuildEvent(this, openRecord));
-  }
-
-  @Transactional(rollbackFor = Exception.class)
-  public void lock() throws Exception {
-    // 查询最新的可投注数据
-    OpenRecord openRecord =
-        openRecordService.getOne(
-            new LambdaQueryWrapper<OpenRecord>()
-                .eq(OpenRecord::getStatus, OpenRecordStatus.Ready)
-                .orderByDesc(OpenRecord::getIssue)
-                .last("limit 1"));
-
-    openRecord.setStatus(OpenRecordStatus.Lock.name());
-    openRecord.setUpdateTime(new Date());
-    openRecordService.updateById(openRecord);
-  }
+        openRecord.setStatus(OpenRecordStatus.Lock.name());
+        openRecord.setUpdateTime(new Date());
+        openRecordService.updateById(openRecord);
+    }
 }
