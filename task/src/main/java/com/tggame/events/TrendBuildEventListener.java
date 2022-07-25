2285b0a28a77bcdc439c0fd9e2cf4a0b2ae2f3f5
watson <watson@cg.support>
2022-07-25 14:17:49 +0800

    计算位数

--- task/src/main/java/com/tggame/events/TrendBuildEventListener.java
+++ task/src/main/java/com/tggame/events/TrendBuildEventListener.java
@@ -31,20 +31,27 @@ public class TrendBuildEventListener {
             OpenRecord openRecord = trendBuildEvent.getOpenRecord();
 
             //计算大小单双
-            String lastNum = openRecord.getNum().split("\\.")[1];
-            String bigSmall = Integer.parseInt(lastNum) < 5 ? "小" : "大";
-            String oddEven = Integer.parseInt(lastNum) % 2 == 0 ? "双" : "单";
-            String data = lastNum + "|" + bigSmall + "|" + oddEven;
+            String[] arr = openRecord.getNum().replace(".", "").split("");
+            int sum = 0;
+            for (String s : arr) {
+                sum += Integer.parseInt(s);
+            }
+
+//            String lastNum = openRecord.getNum().split("\\.")[1];
+//            String bigSmall = Integer.parseInt(lastNum) < 5 ? "小" : "大";
+//            String oddEven = Integer.parseInt(lastNum) % 2 == 0 ? "双" : "单";
+//            String data = lastNum + "|" + bigSmall + "|" + oddEven;
             trendRecordService.save(TrendRecord.builder()
                     .lotteryId(openRecord.getLotteryId())
                     .issue(openRecord.getIssue())
-                    .data(data)
+                    .data(String.valueOf(sum))
                     .openTime(DateUtil.format(new DateTime(), "HH:mm"))
                     .build());
         } catch (Exception e) {
             log.error("-{}", e.getLocalizedMessage(), e);
         }
     }
+
 
 }
 
