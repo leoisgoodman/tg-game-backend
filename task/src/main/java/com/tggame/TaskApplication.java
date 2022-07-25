7f809322511a480ef806ea13d8e26487f93c7431
kotlin <fourtapat@gmail.com>
2022-07-25 20:39:07 +0800

    開啟支持異步

--- task/src/main/java/com/tggame/TaskApplication.java
+++ task/src/main/java/com/tggame/TaskApplication.java
@@ -4,10 +4,12 @@ import com.tggame.ws.BTCWebSocketClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableAsync
 public class TaskApplication {
 
     public static void main(String[] args) {
