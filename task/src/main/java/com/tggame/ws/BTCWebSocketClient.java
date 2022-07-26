e35a8cb2621cbd9972f8bd407af7e7005cd864b7
watson <watson@cg.support>
2022-07-26 17:33:10 +0800

    注釋掉無用的調度

--- task/src/main/java/com/tggame/ws/BTCWebSocketClient.java
+++ task/src/main/java/com/tggame/ws/BTCWebSocketClient.java
@@ -18,71 +18,74 @@ import java.net.URISyntaxException;
 @Component
 public class BTCWebSocketClient extends WebSocketClient {
 
-  private static String url = "wss://stream.binance.com/stream";
+    private static String url = "wss://stream.binance.com/stream";
 
-  /** 此处是解决无法注入的关键 */
-  private static ApplicationContext applicationContext;
+    /**
+     * 此处是解决无法注入的关键
+     */
+    private static ApplicationContext applicationContext;
 
-  public static void setApplicationContext(ApplicationContext applicationContext) {
-    BTCWebSocketClient.applicationContext = applicationContext;
-  }
-
-  public BTCWebSocketClient() throws URISyntaxException {
-    super(new URI(url));
-  }
-
-  @Override
-  public void onOpen(ServerHandshake shake) {
-    log.info("握手...");
-  }
-
-  @Override
-  public void onMessage(String paramString) {
-    log.info("收到的消息为{}", paramString);
-    this.processMessage(paramString);
-  }
-
-  @Override
-  public void onClose(int paramInt, String paramString, boolean paramBoolean) {
-    log.info("关闭...");
-    // todo  要重试
-  }
-
-  @Override
-  public void onError(Exception e) {
-    log.info("异常...");
-
-    //// todo  要重试
-  }
-
-  /**
-   * 私有方法 收到消息后的处理
-   *
-   * @param paramString
-   */
-  private void processMessage(String paramString) {
-    if (paramString.contains("btcusdt@kline_1m")) {
-      JSONObject obj = JSONObject.parseObject(paramString);
-      String data = obj.getString("data");
-      JSONObject objData = JSONObject.parseObject(data);
-      String dataK = objData.getString("k");
-      JSONObject objDataK = JSONObject.parseObject(dataK);
-      Long beginTime = objDataK.getLong("t");
-      Integer minute = DateUtil.minute(DateUtil.date(beginTime));
-      if (minute % 2 == 1) {
-        return;
-      }
-      String btcKey = RedisKey.genBTCKey(beginTime);
-      RedisServiceSVImpl redisServiceSV = applicationContext.getBean(RedisServiceSVImpl.class);
-      if (redisServiceSV.hasKey(btcKey)) {
-        return;
-      }
-      String btcValue = objDataK.getString("o");
-      String value = btcValue.substring(0, btcValue.indexOf(".") + 2);
-      redisServiceSV.set(btcKey, value, RedisKey.BTC.time);
-      log.debug("redis的key:{},值为:{}", btcKey, value);
-      OpenLotteryService openLotteryService = applicationContext.getBean(OpenLotteryService.class);
-      openLotteryService.open(value);
+    public static void setApplicationContext(ApplicationContext applicationContext) {
+        BTCWebSocketClient.applicationContext = applicationContext;
     }
-  }
+
+    public BTCWebSocketClient() throws URISyntaxException {
+        super(new URI(url));
+    }
+
+    @Override
+    public void onOpen(ServerHandshake shake) {
+        log.info("握手...");
+    }
+
+    @Override
+    public void onMessage(String paramString) {
+        log.debug("收到的消息为{}", paramString);
+        this.processMessage(paramString);
+    }
+
+    @Override
+    public void onClose(int paramInt, String paramString, boolean paramBoolean) {
+        log.info("关闭...");
+        // todo  要重试
+    }
+
+    @Override
+    public void onError(Exception e) {
+        log.info("异常...");
+
+        //// todo  要重试
+    }
+
+    /**
+     * 私有方法 收到消息后的处理
+     *
+     * @param paramString
+     */
+    private void processMessage(String paramString) {
+        if (paramString.contains("btcusdt@kline_1m")) {
+            JSONObject obj = JSONObject.parseObject(paramString);
+            String data = obj.getString("data");
+            JSONObject objData = JSONObject.parseObject(data);
+            String dataK = objData.getString("k");
+            JSONObject objDataK = JSONObject.parseObject(dataK);
+            Long beginTime = objDataK.getLong("t");
+            Integer minute = DateUtil.minute(DateUtil.date(beginTime));
+            if (minute % 2 == 1) {
+                return;
+            }
+            String btcKey = RedisKey.genBTCKey(beginTime);
+            RedisServiceSVImpl redisServiceSV = applicationContext.getBean(RedisServiceSVImpl.class);
+            if (redisServiceSV.hasKey(btcKey)) {
+                return;
+            }
+            String btcValue = objDataK.getString("o");
+            String value = btcValue.substring(0, btcValue.indexOf(".") + 2);
+            redisServiceSV.set(btcKey, value, RedisKey.BTC.time);
+            log.debug("redis的key:{},值为:{}", btcKey, value);
+            OpenLotteryService openLotteryService = applicationContext.getBean(OpenLotteryService.class);
+            log.info("奖源数字为-{}", value);
+            openLotteryService.open(value);
+        }
+    }
 }
